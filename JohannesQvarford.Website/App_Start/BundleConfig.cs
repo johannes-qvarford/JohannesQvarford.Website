18d574d23b22798b39d7bc7b6e7517c91b272d95
Johannes Qvarford <johannes.qvarford@gmail.com>
2016-01-10 17:52:36 +0100

    Refactored styles into page specific files, and added layout for projects.

--- JohannesQvarford.Website/App_Start/BundleConfig.cs
+++ JohannesQvarford.Website/App_Start/BundleConfig.cs
@@ -25,6 +25,11 @@
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/project").Include(
+                      "~/Content/project.css"
+                ));
+            bundles.Add(new StyleBundle("~/Content/about").Include(
+                       "~/Content/about.css"));
         }
     }
 }
